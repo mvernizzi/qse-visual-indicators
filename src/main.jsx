00df786b1bbeac3887cd03a353ff82bb44f14b39
mvernizzi <mvernizzi@dislaub.com>
2026-07-31 14:42:28 +0000

    Initialisation iframe Trello

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+if (window.self !== window.top && window.TrelloPowerUp) {
+  window.TrelloPowerUp.iframe();
+}
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
+if (window.self !== window.top && window.TrelloPowerUp) {
+  window.TrelloPowerUp.iframe();
+}
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
