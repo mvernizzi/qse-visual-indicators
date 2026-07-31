d55d388e8ae5a0e9b50a04671655286f46d0529d
mvernizzi <mvernizzi@dislaub.com>
2026-07-31 14:31:01 +0000

    Correction du chemin du connecteur Trello

--- public/connector.js
+++ public/connector.js
@@ -1,18 +1,16 @@
 window.TrelloPowerUp.initialize({
-
   "card-back-section": function (t) {
-
     return {
       title: "Indicateurs QSE",
-      icon: "https://mvernizzi.github.io/qse-visual-indicators/favicon.svg",
+      icon:
+        "https://mvernizzi.github.io/qse-visual-indicators/favicon.svg",
       content: {
         type: "iframe",
         url: t.signUrl(
-  "https://mvernizzi.github.io/qse-visual-indicators/"
-)
+          "https://mvernizzi.github.io/qse-visual-indicators/"
+        ),
+        height: 1200
       }
     };
-
   }
-
 });
